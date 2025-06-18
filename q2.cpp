19819f84307daad65c9eb02cd9195d676097fc47
sh-gun14 <shagunhudda1409@gmail.com>
2025-06-19 03:36:24 +0530

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -1,5 +1,4 @@
-#include <bits/stdc++.h>
-using namespace std;
+
 
 int findMin(vector<int>& a) {
     int l = 0, r = a.size() - 1;
@@ -11,10 +10,4 @@ int findMin(vector<int>& a) {
     return a[l];
 }
 
-int main() {
-    int n; cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; ++i) cin >> a[i];
-    cout << findMin(a) << endl;
-    return 0;
-}
+
