a3db41ed9c8a766c3e8099ba8e35b048675c77ec
sh-gun14 <shagunhudda1409@gmail.com>
2025-06-19 03:34:55 +0530

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -1,5 +1,4 @@
-#include <bits/stdc++.h>
-using namespace std;
+
 
 
 int countOccurrences(vector<int>& a, int target) {
@@ -8,11 +7,3 @@ int countOccurrences(vector<int>& a, int target) {
     return high - low;
 }
 
-int main() {
-    int n, target;
-    cin >> n >> target;
-    vector<int> a(n);
-    for (int i = 0; i < n; ++i) cin >> a[i];
-    cout << countOccurrences(a, target) << endl;
-    return 0;
-}
